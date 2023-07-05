551cc0a8fff3aff01ecf984649a90e573b776091
se0jinYoon <tjwls1999@gmail.com>
2023-07-05 16:08:30 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,13 +1,36 @@
-import Button from "./Button.js";
-import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes")
+  },[keyword])
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes")
+  },[counter])
+
+
   return (
     <div>
-      <h1 className={styles.title}>Wecome back!</h1>
-      <Button text={"continue"}/>
+      <input 
+        value = {keyword} 
+        onChange = {onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button onClick = {onClick}>click me</button>
     </div>
-  );
-}
-
+  )
+    
+} 
 export default App;
