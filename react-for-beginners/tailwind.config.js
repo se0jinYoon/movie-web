1cb08f5f9181ea69b7b475ed5621b39947fe1469
se0jinYoon <tjwls1999@gmail.com>
2023-07-10 17:32:29 +0900

    tailwind

---
+++ react-for-beginners/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,tx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
+
